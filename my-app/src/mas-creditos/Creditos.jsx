e80e57152be7e532775fb9d261cc1b8e0f9b917f
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-05-30 16:32:11 -0300

    radio button para sumar creditos

--- my-app/src/mas-creditos/Creditos.jsx
+++ my-app/src/mas-creditos/Creditos.jsx
@@ -17,6 +17,12 @@ function Creditos(){
                         <input className="input user" placeholder="Usuario" type="text"/>
                         <input className="input pass" placeholder="Contraseña" type="text"/>
                         <div className="btn-container">
+                            <input type="radio" id="1000" name="credit" value="1000" />
+                            <label for="1000">1000</label>
+                            <input type="radio" id="5000" name="credit" value="5000" />
+                            <label for="5000">5000</label>
+                            <input type="radio" id="7500" name="credit" value="7500" />
+                            <label for="7500">7500</label>
                         </div>
                         <button>Canjear</button>
                     </form>
