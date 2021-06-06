7f018f291e99fd05c68bfcccab9d93c4dcd82917
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-06 20:27:24 -0300

    fix css button pagination

--- my-app/src/productos/Paginador.jsx
+++ my-app/src/productos/Paginador.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import next from '../images/icons/arrow-right.svg'
+import prev from '../images/icons/arrow-left.svg'
+
 
 function Paginador({productList, pagination,setPagination}){
 
     return(
-        <div>
+        <div className="pagination">
             {productList.length > 15 &&
                 (pagination ? (
-                    <button onClick={()=>setPagination(!pagination)}>Siguente</button>
+                    <>
+                    <button onClick={()=>setPagination(!pagination)}>
+                        Siguente <img src={next} alt="next"/>
+                    </button>
+                    </>
                 ) : (
-                    <button onClick={()=>setPagination(!pagination)}>Anterior</button>
+                    <button onClick={()=>setPagination(!pagination)}>
+                        <img src={prev} alt="prev"/>Anterior
+                        </button>
             ))}
         </div>
     )
