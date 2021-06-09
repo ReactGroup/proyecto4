f6a5b15c11cb5a96441248ed6063dd4983d847ab
Tomi_SS <dg.sanchezsoria@gmail.com>
2021-06-08 22:06:44 -0300

    cambio de router en index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import CoinProvider from './coinProvider/CoinProvider'
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -10,10 +10,10 @@ import ScrollToTop from "./scrollToTop";
 ReactDOM.render(
   <React.StrictMode>
     <CoinProvider>
-      <BrowserRouter>
+      <HashRouter>
       <ScrollToTop />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CoinProvider>
   </React.StrictMode>,
   document.getElementById('root')
